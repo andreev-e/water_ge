d685bb3f88bf930fb0c1f207f26bc02c80ac6dae
e.andreev <k9prP46sXJ>
2023-05-08 09:13:25 +0300

    Telegram buttons

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -5,8 +5,6 @@ namespace App\Telegram\Commands;
 
 
 use Longman\TelegramBot\Commands\UserCommand;
-use Longman\TelegramBot\Entities\InlineKeyboard;
-use Longman\TelegramBot\Entities\InlineKeyboardButton;
 use Longman\TelegramBot\Entities\Keyboard;
 use Longman\TelegramBot\Entities\ServerResponse;
 
@@ -30,26 +28,23 @@ class StartCommand extends UserCommand
     {
         $languageCode = $this->getMessage()->getFrom()->getLanguageCode();
 
-//        $keyboardInline = new InlineKeyboard([]);
-//        $keyboardInline->addRow(
-//            (new InlineKeyboardButton(__('telegram.buttons.search', locale: $languageCode)))
-//                ->setCallbackData(json_encode([
-//                    'name' => 'search',
-//                    'show' => true,
-//                ], JSON_THROW_ON_ERROR)),
-//            (new InlineKeyboardButton(__('telegram.buttons.list', locale: $languageCode)))
-//                ->setCallbackData(json_encode([
-//                    'name' => 'list',
-//                    'show' => true,
-//                ], JSON_THROW_ON_ERROR))
-//        );
-
         return $this->replyToChat(
             __('telegram.start', locale: $languageCode),
             [
                 'parse_mode' => 'markdown',
-                'reply_markup' => [],
-                'one_time_keyboard' => true,
+                'reply_markup' => new Keyboard(
+                    [
+                        'keyboard' => [
+                            [
+                                __('telegram.buttons.search', locale: $languageCode),
+                                __('telegram.buttons.list', locale: $languageCode),
+                            ],
+                        ],
+                        'resize_keyboard' => true,
+                        'one_time_keyboard' => true,
+                        'selective' => true,
+                    ]
+                ),
             ]);
     }
 }
