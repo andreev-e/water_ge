615b39da17feb2e28743556a73b202cd5f6d1e7a
e.andreev <k9prP46sXJ>
2023-05-08 09:21:15 +0300

    Telegram buttons

---
+++ app/Telegram/Commands/SeachCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Telegram\Commands;
+
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Entities\Keyboard;
+use Longman\TelegramBot\Entities\ServerResponse;
+
+class SeachCommand extends UserCommand
+{
+
+    /** @var string Command name */
+    protected $name = 'search';
+    /** @var string Command description */
+    protected $description = 'Search';
+    /** @var string Usage description */
+    protected $usage = '/search';
+    /** @var string Version */
+    protected $version = '1.0.0';
+
+    /**
+     * @throws \Longman\TelegramBot\Exception\TelegramException
+     * @throws \JsonException
+     */
+    public function execute(): ServerResponse
+    {
+        $languageCode = $this->getMessage()->getFrom()->getLanguageCode();
+
+        return $this->replyToChat('Search command');
+    }
+}
