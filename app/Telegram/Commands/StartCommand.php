d26b0a6b9f20887fdb46a523c7b03cc25cddb027
e.andreev <k9prP46sXJ>
2023-05-08 09:09:44 +0300

    Telegram buttons

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -30,25 +30,25 @@ class StartCommand extends UserCommand
     {
         $languageCode = $this->getMessage()->getFrom()->getLanguageCode();
 
-        $keyboardInline = new InlineKeyboard([]);
-        $keyboardInline->addRow(
-            (new InlineKeyboardButton(__('telegram.buttons.search', locale: $languageCode)))
-                ->setCallbackData(json_encode([
-                    'name' => 'search',
-                    'show' => true,
-                ], JSON_THROW_ON_ERROR)),
-            (new InlineKeyboardButton(__('telegram.buttons.list', locale: $languageCode)))
-                ->setCallbackData(json_encode([
-                    'name' => 'list',
-                    'show' => true,
-                ], JSON_THROW_ON_ERROR))
-        );
+//        $keyboardInline = new InlineKeyboard([]);
+//        $keyboardInline->addRow(
+//            (new InlineKeyboardButton(__('telegram.buttons.search', locale: $languageCode)))
+//                ->setCallbackData(json_encode([
+//                    'name' => 'search',
+//                    'show' => true,
+//                ], JSON_THROW_ON_ERROR)),
+//            (new InlineKeyboardButton(__('telegram.buttons.list', locale: $languageCode)))
+//                ->setCallbackData(json_encode([
+//                    'name' => 'list',
+//                    'show' => true,
+//                ], JSON_THROW_ON_ERROR))
+//        );
 
         return $this->replyToChat(
             __('telegram.start', locale: $languageCode),
             [
                 'parse_mode' => 'markdown',
-                'reply_markup' => $keyboardInline->getRawData(),
+                'reply_markup' => [],
                 'one_time_keyboard' => true,
             ]);
     }
