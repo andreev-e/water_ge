a67f526306b2eae96ec0ba79d0bc1eb0f53af5f1
e.andreev <k9prP46sXJ>
2023-05-18 14:22:08 +0300

    Shorter dates

--- resources/lang/en/telegram.php
+++ resources/lang/en/telegram.php
@@ -4,5 +4,5 @@ return [
     'water_shutdown' => 'Water shutdown',
     'default_answer' => 'Can\'t help you.',
     'actual_shutdowns' => 'Actual shutdowns',
-    'start' => 'Hi there! This bot can notify you about upcoming water shutdowns',
+    'start' => 'Hi there! This bot can notify you about upcoming water shutdowns. Write me anything to get current shutdowns list',
 ];
