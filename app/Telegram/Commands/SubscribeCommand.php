bd5b7237f6340c5182486ba65c1ab6b71388175e
e.andreev <k9prP46sXJ>
2023-05-20 18:30:38 +0300

    Callback test

--- app/Telegram/Commands/SubscribeCommand.php
+++ app/Telegram/Commands/SubscribeCommand.php
@@ -7,6 +7,7 @@ use App\Models\ServiceCenter;
 use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Entities\CallbackQuery;
 use Longman\TelegramBot\Entities\InlineKeyboard;
+use Longman\TelegramBot\Entities\InlineKeyboardButton;
 use Longman\TelegramBot\Entities\KeyboardButton;
 use Longman\TelegramBot\Entities\ServerResponse;
 
@@ -23,17 +24,17 @@ class SubscribeCommand extends UserCommand
 
         $buttons = [];
         foreach (ServiceCenter::all() as $serviceCenter) {
-            $button = new KeyboardButton([
+            $button = new InlineKeyboardButton([
                 'text' => $serviceCenter->name_ru,
                 'callback_data' => 'command=subscribe&serviceCenter=' . $serviceCenter->id,
             ]);
-            $buttons[] = [$button];
+            $buttons[] = $button;
         }
 
         $keyboard = new InlineKeyboard($buttons);
 
         return $this->replyToChat(
-            __('telegram.start', locale: $languageCode),
+            __('telegram.buttons.set_city', locale: $languageCode),
             [
                 'reply_markup' => $keyboard,
             ]
