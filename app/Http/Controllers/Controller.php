25e917a5210478ca32d1bdb77075b38ee4c27c60
e.andreev <k9prP46sXJ>
2023-05-21 09:13:56 +0300

    Speed optimization

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -18,58 +18,62 @@ class Controller extends BaseController
     {
         $currentEvents = Event::getCurrent();
 
-        $serviceCenters = ServiceCenter::query()
-            ->orderBy('total_events', 'DESC')
-            ->limit(10)
-            ->get();
+        $graphData = Cache::remember('graphdata', 60 * 60, function() {
 
-        $events = Event::query()
-            ->with(['serviceCenter', 'addresses'])
-            ->where('start', '>=', now()->subDays(120))
-            ->orderBy('start')
-            ->get();
+            $events = Event::query()
+                ->with(['serviceCenter', 'addresses'])
+                ->where('start', '>=', now()->subDays(120))
+                ->orderBy('start')
+                ->get();
 
-        $graphData = [];
-        $graphData['labels'] = [];
-        $graphData['datasets'] = [];
+            $graphData = [];
+            $graphData['labels'] = [];
+            $graphData['datasets'] = [];
 
-        foreach ($events as $event) {
-            $date = $event->start->format('d.m.Y');
-            if (!in_array($date, $graphData['labels'], true)) {
-                $graphData['labels'][] = $date;
+            foreach ($events as $event) {
+                $date = $event->start->format('d.m.Y');
+                if (!in_array($date, $graphData['labels'], true)) {
+                    $graphData['labels'][] = $date;
+                }
             }
-        }
 
-        foreach ($serviceCenters as $serviceCenter) {
-            $color = $this->rand_color();
-            $graphData['datasets'][$serviceCenter->id]['label'] = $serviceCenter->name_ru;
-            $graphData['datasets'][$serviceCenter->id]['backgroundColor'] = $color;
-            $graphData['datasets'][$serviceCenter->id]['borderColor'] = $color;
-            $graphData['datasets'][$serviceCenter->id]['fill'] = false;
-        }
+            $serviceCenters = ServiceCenter::query()
+                ->orderBy('total_events', 'DESC')
+                ->limit(10)
+                ->get();
 
-        foreach ($graphData['labels'] as $date) {
             foreach ($serviceCenters as $serviceCenter) {
-                $found = false;
-                foreach ($events as $event) {
-                    if ($date === $event->start->format('d.m.Y') && $serviceCenter->id === $event->serviceCenter->id) {
-                        $found = true;
-                        $number = $serviceCenter->total_addresses - $event->total_addresses;
-                        $graphData['datasets'][$serviceCenter->id]['data'][] = $number;
-                        break;
+                $color = $this->rand_color();
+                $graphData['datasets'][$serviceCenter->id]['label'] = $serviceCenter->name_ru;
+                $graphData['datasets'][$serviceCenter->id]['backgroundColor'] = $color;
+                $graphData['datasets'][$serviceCenter->id]['borderColor'] = $color;
+                $graphData['datasets'][$serviceCenter->id]['fill'] = false;
+            }
+
+            foreach ($graphData['labels'] as $date) {
+                foreach ($serviceCenters as $serviceCenter) {
+                    $found = false;
+                    foreach ($events as $event) {
+                        if ($date === $event->start->format('d.m.Y') && $serviceCenter->id === $event->serviceCenter->id) {
+                            $found = true;
+                            $number = $serviceCenter->total_addresses - $event->total_addresses;
+                            $graphData['datasets'][$serviceCenter->id]['data'][] = $number;
+                            break;
+                        }
+                    }
+                    if (!$found) {
+                        $graphData['datasets'][$serviceCenter->id]['data'][] = $serviceCenter->total_addresses;
                     }
                 }
-                if (!$found) {
-                    $graphData['datasets'][$serviceCenter->id]['data'][] = $serviceCenter->total_addresses;
-                }
             }
-        }
 
-        $graphData['datasets'] = array_values($graphData['datasets']);
+            $graphData['datasets'] = array_values($graphData['datasets']);
+
+            return $graphData;
+        });
 
         return view('welcome', compact([
             'currentEvents',
-            'serviceCenters',
             'graphData',
         ]));
     }
