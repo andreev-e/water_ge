59b3adb1271657a883b7b4808b66a8e79cd4c412
e.andreev <k9prP46sXJ>
2023-06-07 08:44:25 +0300

    Layout

--- resources/views/table_row.blade.php
+++ resources/views/table_row.blade.php
@@ -12,6 +12,15 @@
         <a class="text-cyan-600" href="/?service_center_id={{ $event->serviceCenter->id }}">
             {{ $event->serviceCenter->name_ru }}
         </a>
+        @if($withLink)
+            <a
+                href="{{ route('event', ['event' => $event->id]) }}"
+                class="btn bg-slate-200 px-2 py-0.5 rounded"
+                title="Подробнее"
+            >
+                ...
+            </a>
+        @endif
     </td>
     <td class="px-1">
         @if ($event->serviceCenter->total_addresses && $event->type !== EventTypes::gas)
@@ -25,15 +34,4 @@
     <td class="px-1">{{ $event->start->diffForHumans() }}</td>
     <td class="px-1">{{ $event->start < Carbon::now() ? $event->finish->diffForHumans(): $event->finish->diffForHumans($event->start)  }}</td>
     <td class="px-1">{{ $event->from_to }}</td>
-
-    @if($withLink)
-        <td class="p-1">
-            <a
-                href="{{ route('event', ['event' => $event->id]) }}"
-                class="btn bg-slate-200 px-2 py-0.5 rounded"
-            >
-                Смотреть
-            </a>
-        </td>
-    @endif
 </tr>
