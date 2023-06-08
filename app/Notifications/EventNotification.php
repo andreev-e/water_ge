ad70d555e8167f4f4947c2a34798f6d591da5760
e.andreev <k9prP46sXJ>
2023-06-08 10:01:40 +0300

    Less lines in message

--- app/Notifications/EventNotification.php
+++ app/Notifications/EventNotification.php
@@ -13,7 +13,7 @@ class EventNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
-    const SHOW_IN_MESSAGE = 20;
+    const SHOW_IN_MESSAGE = 10;
 
     public function __construct(
         public Event $event,
