f138292434b8c4be479dafbc9a006e908eb40719
e.andreev <k9prP46sXJ>
2024-01-04 18:36:53 +0300

    Ok

--- app/Console/Commands/MakeMailNotSubscribed.php
+++ app/Console/Commands/MakeMailNotSubscribed.php
@@ -15,9 +15,6 @@ class MakeMailNotSubscribed extends Command
     protected $signature = 'app:make-mail-not-subscribed';
     protected $description = 'Notifies users with 0 subscriptions';
 
-    /**
-     * @throws \JsonException
-     */
     public function handle()
     {
         $ids = BotUser::query()
@@ -29,11 +26,11 @@ class MakeMailNotSubscribed extends Command
         $text = __('telegram.mail_not_subscribed', [], 'ru');
 
         if ($ids->count()) {
-//            Mail::query()->create([
-//                'text' => $text,
-//                'to' => $ids,
-//                'status' => MailStatuses::new,
-//            ]);
+            Mail::query()->create([
+                'text' => $text,
+                'to' => $ids,
+                'status' => MailStatuses::new,
+            ]);
 
             Mail::query()->create([
                 'text' => 'Уведомил не подписанных на рассылку ' . $ids->count(),
